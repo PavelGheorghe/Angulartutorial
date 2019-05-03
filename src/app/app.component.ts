a3753a1e951d2bae9499c33a7f2a8fe14910b13d
Pavel Gheorghe <g.pavel@software-dep.net>
2019-05-03 17:31:07 +0300

    template form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {HttpClient}  from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 
 import {MyserviceService} from './myservice.service';
 
@@ -10,22 +10,17 @@ import {MyserviceService} from './myservice.service';
 })
 export class AppComponent {
   title = 'Welcome to Angular tutorialSpoint';
-  months = ['January','February', 'March','April','May', 'March','April','May', 'March','April','May',]
+  months = ['January', 'February', 'March', 'April', 'May', 'March', 'April', 'May', 'March', 'April', 'May'];
   isavailable = true;
   httpdata;
-  todaydate;  
-  constructor(private date:MyserviceService ,private http:HttpClient){
-
-  }
-    ngOnInit(){
+  todaydate;
+    constructor(private date: MyserviceService, private http: HttpClient) { }
+    ngOnInit() {
       this.todaydate = this.date.ShowTodayDate();
       this.title = this.date.sometext;
-      this.date.sometext='valerica';
-
-      this.http.get("http://jsonplaceholder.typicode.com/users")
-      .subscribe((data)=>this.copydata(data));
+      this.date.sometext = 'valerica';
+      this.http.get('http://jsonplaceholder.typicode.com/users')
+      .subscribe((data) => this.copydata(data));
     }
-
-    copydata(data){ this.httpdata=data; }
-  
+    copydata(data) { this.httpdata = data; }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MyserviceService} from './myservice.service'
+import { MyserviceService} from './myservice.service';
 import { NewCompComponent } from './new-comp/new-comp.component';
 import { ChangeTextDirective } from './change-text.directive';
 import { SqrtPipe} from './sqrt';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
     SqrtPipe,
     AppComponent,
     NewCompComponent,
-    ChangeTextDirective
+    ChangeTextDirective,
+    HeroFormComponent
   ],
   imports: [
     HttpClientModule,
+    FormsModule,
     BrowserModule,
     RouterModule.forRoot([{
-         path:'new-component',
-         component:NewCompComponent
+         path: 'new-component',
+         component: NewCompComponent
     }]),
   ],
   providers: [MyserviceService],
